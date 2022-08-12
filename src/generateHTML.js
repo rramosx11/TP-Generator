4f2915e578ea7e5ff82c78a3527959b5be6cc118
rramosx11 <rodolforamosd11@gmail.com>
2022-08-11 23:33:32 -0400

    added the final styles for the card

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -5,7 +5,7 @@ const generateManager = function (manager) {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${manager.name}</h3>
-                <h4>Manager</h4><i class="material-icons">content_paste</i>
+                <h4>Manager</h4><i class="material-icons">supervisor_account</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${manager.id}</p>
@@ -24,7 +24,7 @@ const generateEngineer = function (engineer) {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${engineer.name}</h3>
-                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+                <h4>Engineer</h4><i class="material-icons">engineering</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${engineer.id}</p>
@@ -43,7 +43,7 @@ const generateIntern = function (intern) {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${intern.name}</h3>
-                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+                <h4>Intern</h4><i class="material-icons">school</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${intern.id}</p>
@@ -112,7 +112,7 @@ const generateTeamPage = function (employeeCards) {
 <body>
     <header>
         <nav class="navbar" id="navbar">
-        <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+        <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">My Team</span>
     </nav>
 </header>
 <main>
